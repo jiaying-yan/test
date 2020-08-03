2a80958115faebc51154d12b84315d8a41847bc0
jiaying-yan <65887664+jiaying-yan@users.noreply.github.com>
2020-08-03 17:03:37 +0800

    Update random12.py

--- random12.py
+++ random12.py
@@ -3,6 +3,6 @@ import random
 num = random.choice([1, 2])
 if num == 1:
     payload = {"key1": "value1", "key2": "value2"}
-    r = requests.get("www.baidu.com", param=payload)
+    r = requests.get("http://httpbin.org/get", param=payload)
 else:
     print("current num is 2")
