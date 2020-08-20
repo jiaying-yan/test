94025805c2ac7fb7eb9bf270e87f4ffafa71f399
jiaying-yan <65887664+jiaying-yan@users.noreply.github.com>
2020-08-20 18:03:05 +0800

    Update condition12.py

--- condition12.py
+++ condition12.py
@@ -7,9 +7,9 @@ from pathlib import Path
 def test_list(args):
     parser = argparse.ArgumentParser(description='Returns hive list for python')
     # parser.add_argument("--input", type=str, required=True)
-    parser.add_argument("--output", type=str, required=True)
+    parser.add_argument("--output", type=num, required=True)
     args = parser.parse_args(args)
-    input_data = random.choice(["a", "b"])
+    input_data = random.choice([1, 2])
     Path(args.output).parent.mkdir(parents=True, exist_ok=True)
     with open(args.output, 'w') as output_path:
         output_path.write('{}'.format(input_data))
