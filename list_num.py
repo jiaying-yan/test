a339c54801377c72fe100032d622b5bc46d6678b
jiaying-yan <65887664+jiaying-yan@users.noreply.github.com>
2020-10-22 12:59:14 +0800

    Update list_num.py

--- list_num.py
+++ list_num.py
@@ -5,11 +5,12 @@ from pathlib import Path
 
 
 def test_list(args):
-    parser = argparse.ArgumentParser(description='Returns hive list for python')
+    parser = argparse.ArgumentParser(description='Returns list for python')
     # parser.add_argument("--input", type=str, required=True)
     parser.add_argument("--output", type=str, required=True)
     args = parser.parse_args(args)
-    input_data = json.loads("[1, 2]")
+    result = "[1, 2]"
+    input_data = json.loads(result)
     Path(args.output).parent.mkdir(parents=True, exist_ok=True)
     with open(args.output, 'w') as output_path:
         output_path.write('{}'.format(json.dumps(input_data)))
