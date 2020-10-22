0a320ed2db41a88d540af84b54e3c9d51ac4e346
jiaying-yan <65887664+jiaying-yan@users.noreply.github.com>
2020-10-22 12:09:33 +0800

    Create list_num.py

---
+++ list_num.py
@@ -0,0 +1,22 @@
+import argparse
+import sys
+import json
+from pathlib import Path
+
+
+def test_list(args):
+    parser = argparse.ArgumentParser(description='Returns list which length = input')
+    parser.add_argument("--input", type=num, required=True)
+    parser.add_argument("--output", type=str, required=True)
+    args = parser.parse_args(args)
+    result = []
+    for i in range(0,num):
+      result.append(i)
+    Path(args.output).parent.mkdir(parents=True, exist_ok=True)
+    with open(args.output, 'w') as output_path:
+        output_path.write('{}'.format(json.dumps(result))
+
+
+if __name__ == '__main__':
+
+    test_list(sys.argv[1:])
