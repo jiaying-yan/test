dcc8b491bab98b21ffb54019ba3f3519690e5533
jiaying-yan <65887664+jiaying-yan@users.noreply.github.com>
2020-10-22 12:54:06 +0800

    Update list_num.py

--- list_num.py
+++ list_num.py
@@ -9,10 +9,10 @@ def test_list(args):
     # parser.add_argument("--input", type=num, required=True)
     parser.add_argument("--output", type=str, required=True)
     args = parser.parse_args(args)
-    result = [1,2,3]
+    result = str([1,2,3])
     # for i in range(0,1):
       # result.append(i)
-    result_data = json.loads(str(reuslt))
+    result_data = json.loads(reuslt)
     Path(args.output).parent.mkdir(parents=True, exist_ok=True)
     with open(args.output, 'w') as output_path:
         output_path.write('{}'.format(json.dumps(result_data))
