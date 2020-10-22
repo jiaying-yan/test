e98ad6528d0cf7d7bebdf2b32f8b32f12508a055
jiaying-yan <65887664+jiaying-yan@users.noreply.github.com>
2020-10-22 12:18:48 +0800

    Update list_num.py

--- list_num.py
+++ list_num.py
@@ -6,11 +6,11 @@ from pathlib import Path
 
 def test_list(args):
     parser = argparse.ArgumentParser(description='Returns list which length = input')
-    parser.add_argument("--input", type=num, required=True)
+    # parser.add_argument("--input", type=num, required=True)
     parser.add_argument("--output", type=str, required=True)
     args = parser.parse_args(args)
     result = []
-    for i in range(0,num):
+    for i in range(0,1):
       result.append(i)
     result_data = json.loads(reuslt)
     Path(args.output).parent.mkdir(parents=True, exist_ok=True)
