ec2d2e1cbc1e6a80f6beb5d24fab9b0021cb1c53
jiaying-yan <65887664+jiaying-yan@users.noreply.github.com>
2021-04-12 16:20:54 +0800

    Update train2.py

--- train2.py
+++ train2.py
@@ -33,13 +33,12 @@ def main(args):
     # Read the wine-quality csv file from the URL
     parser = argparse.ArgumentParser(description='Returns sum of two arguments')
     parser.add_argument("--env", type=str, required=True)
-    # parser.add_argument("--file", type=file, required=True)
+    parser.add_argument("--file", type=file, required=True)
     # parser.add_argument("--env_output", type=str, required=True)
     args = parser.parse_args(args)
     env = args.env
-    # csv_url = args.file
-    csv_url =\
-        'http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
+    csv_url = args.file
+    # csv_url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
     try:
         data = pd.read_csv(csv_url, sep=';')
     except Exception as e:
